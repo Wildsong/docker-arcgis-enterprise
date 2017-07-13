93e5a931d8d89882b26a37364ef8621aa2bcaad5
Brian Wilson <brian@wildsong.biz>
2017-07-12 19:11:51 -0700

    config step added

--- datastore/start.sh
+++ datastore/start.sh
@@ -2,7 +2,29 @@
 # Fix up the name of our properties file
 ln -s .ESRI.properties.*.${ESRI_VERSION} .ESRI.properties.${HOSTNAME}.${ESRI_VERSION}
 
-# Start up the datastore server!
+ARCGISSERVER="server"
+USER="siteadmin"
+PASSWD="changeit"
+
+echo "Starting up the datastore server!"
 ./datastore/startdatastore.sh
 
+sleep 5
 
+echo "Waiting for Datastore server \"${HOSTNAME}\"..."
+curl --retry 15 -sS --insecure "https://${HOSTNAME}:2443/" > /tmp/dshttp
+if [ $? != 0 ]; then
+    echo "Datastore did not start. $?"
+    exit 1
+fi
+
+echo "Connecting datastore to \"${ARCGISSERVER\"."
+cd datastore/tools
+./configuredatastore.sh https://${ARCGISSERVER}:6443/ ${USER} ${PASSWD} \
+			${DS_DATADIR} \
+			--stores relational
+# [--stores [relational][,][tileCache][,][spatiotemporal]]
+
+echo "Configuration is now done, theoretically."
+
+exit 0
